7c1552fb5d511f7ce0fd6c1f7018ffe1b86c803c
Simon M <git@simon.marache.net>
2020-03-08 12:17:53 +0000

    Add support for moving camera at an angle and improved rendering performance

--- src/main.rs
+++ src/main.rs
@@ -52,11 +52,11 @@ fn main() {
     let mut frame_ctr = stats::Stats::default();
     let mut draw_ctr = stats::Stats::default();
 
-    let mut camera_location = (240, 277, 150);
+    let mut camera_location = (240., 277., 150.);
+    let mut angle = 0.;
 
     while !request_exit {
         let tick_start_frame = get_ticks();
-
         // Process the events
         loop {
             let evt = poll_event();
@@ -67,12 +67,12 @@ fn main() {
                     println!("keypress: {:?}", k as usize);
                     match k {
                         Escape => request_exit = true,
-                        Key::Left => camera_location.0 -= 5,
-                        Key::Right => camera_location.0 += 5,
-                        Key::Up => camera_location.1 -= 5,
-                        Key::Down => camera_location.1 += 5,
-                        Key::PageUp => camera_location.2 += 5,
-                        Key::PageDown => camera_location.2 -= 5,
+                        Key::Left => angle += 0.1,
+                        Key::Right => angle -= 0.1,
+                        Key::Up => camera_location.1 -= 5.,
+                        Key::Down => camera_location.1 += 5.,
+                        Key::PageUp => camera_location.2 += 5.,
+                        Key::PageDown => camera_location.2 -= 5.,
                         _ => (),
                     }
                 }
@@ -81,7 +81,7 @@ fn main() {
         }
         screen.clear();
         let tick_start_draw = get_ticks();
-        draw(&screen, &map, &texture, camera_location);
+        draw(&screen, &map, &texture, camera_location, angle);
         draw_ctr.add(get_ticks() - tick_start_draw);
 
         screen.flip();
@@ -104,9 +104,9 @@ fn set_color(image: &mut [u8], i: usize, j: usize, pitch: usize, value: RGBA8) {
     image[pixel_offset + 2] = value.b;
 }
 
-fn draw_line(image: &mut [u8], i: usize, jmin: usize, jmax: usize, pitch: usize, value: RGBA8) {
+fn draw_line(image: &mut [u8], i: usize, jmin: usize, jmax: usize, image_h: usize, pitch: usize, value: RGBA8) {
     for height in jmin..jmax {
-        set_color(image, i, height, pitch, value);
+        set_color(image, i, image_h - height - 1, pitch, value);
     }
 }
 
@@ -114,7 +114,8 @@ fn draw(
     screen: &Surface,
     map: &terrain::HeightMap,
     texture: &terrain::Texture,
-    camera_location: (i32, i32, i32),
+    camera_location: (f32, f32, f32),
+    angle: f32,
 ) {
     let (screen_w, screen_h) = screen.get_size();
     let screen_w = screen_w as i32;
@@ -123,35 +124,64 @@ fn draw(
     let pitch = get_pitch(&screen) as usize;
 
     let horizon = 120.;
-    let scale_height = 120.;
+    let scale_height = 60.;
     let distance_max = 300;
 
-    screen.with_lock(|screen_pixels| {
-        for z in (1..distance_max).rev() {
-            let left = (camera_location.0 - z, camera_location.1 - z);
-            let right = (camera_location.0 + z, camera_location.1 - z);
+    let cos_angle = angle.cos();
+    let sin_angle = angle.sin();
 
-            let stride = (right.0 - left.0) as f32 / screen_w as f32;
+    screen.with_lock(|screen_pixels| {
+        let mut max_height = Vec::new();
+        max_height.resize(screen_w as usize, 0);
+
+        for z in 1..distance_max {
+            let z = z as f32;
+            let left = (
+                (-cos_angle * z - sin_angle * z) + camera_location.0,
+                (sin_angle * z - cos_angle * z) + camera_location.1
+            );
+
+            let right = (
+                (cos_angle * z - sin_angle * z) + camera_location.0,
+                (-sin_angle * z - cos_angle * z) + camera_location.1
+            );
+
+            let stride = (
+                (right.0 - left.0) / screen_w as f32,
+                (right.1 - left.1) / screen_w as f32
+            );
 
             for i in 0..screen_w as usize {
-                let height_on_hm = map.get((left.0 as f32 + i as f32 * stride) as i32, left.1);
+                let i_float = i as f32;
+                let height_on_hm = map.get(
+                    (left.0 + i_float * stride.0) as i32,
+                    (left.1 + i_float * stride.1) as i32,
+                );
 
-                let texture_value = texture.get((left.0 as f32 + i as f32 * stride) as i32, left.1);
+                let texture_value = texture.get(
+                    (left.0 + i_float * stride.0) as i32,
+                    (left.1 + i_float * stride.1) as i32,
+                );
 
                 let real_height = (height_on_hm as i32 - camera_location.2 as i32) as f32
                     / z as f32
                     * scale_height
                     + horizon;
+
                 let real_height = max(0, real_height as i32) as usize;
 
-                draw_line(
-                    screen_pixels,
-                    i,
-                    screen_h - min(screen_h, real_height),
-                    screen_h,
-                    pitch,
-                    texture_value,
-                );
+                if real_height > max_height[i] {
+                    draw_line(
+                        screen_pixels,
+                        i,
+                        max_height[i],
+                        min(real_height, screen_h),
+                        screen_h,
+                        pitch,
+                        texture_value,
+                    );
+                    max_height[i] = real_height
+                }
             }
         }
         true
